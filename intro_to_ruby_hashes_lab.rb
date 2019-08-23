03e18635bcd51956dbaa0c1411f3ce13cf00b881
Alyssa Yates <bosslys@icloud.com>
2019-08-23 02:08:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,10 +40,10 @@ def monopoly_with_fourth_tier
 		:pieces => 4 ,
 		:names => {
 			:reading_railroad => {mortgage_value: "$100"} ,
-			:pennsylvania_railroad =>
-			:b_and_o_railroad =>
-			:shortline_railroad => 
+			:pennsylvania_railroad => {} ,
+			:b_and_o_railroad => {} ,
+			:shortline_railroad => {} ,
 			} ,
-		}
-	}
+		} ,
+	} ,
 end
